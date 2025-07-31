a865b6e2b58a262d774b4630fe151a5fc6c6d445
Alexander Guryanov <caiiiycuk@gmail.com>
2025-07-31 16:27:48 +0300

    switch to release build

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 set -ex
-scp -i ../cloud/etc/yandex-vm cmake-build-debug-clang/peer-server.bin.x86_64  cloud.js-dos.com:~/peer-server.new
+#scp -i ../cloud/etc/yandex-vm cmake-build-debug-clang/peer-server.bin.x86_64  cloud.js-dos.com:~/peer-server.new
+scp -i ../cloud/etc/yandex-vm cmake-build-release-clang/peer-server.bin.x86_64  cloud.js-dos.com:~/peer-server.new
