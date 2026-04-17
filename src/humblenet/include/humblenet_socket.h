79e4d7e85cb75a2cf3e573208d69a4620f0437bb
Alexander Guryanov <caiiiycuk@gmail.com>
2026-04-17 16:46:05 +0300

    hs_select fix for native

--- src/humblenet/include/humblenet_socket.h
+++ src/humblenet/include/humblenet_socket.h
@@ -93,10 +93,31 @@ int hs_sendto( int sock, const void* buffer, int length, int flags, struct socka
 
 int hs_select( int nfds, fd_set *readfds, fd_set *writefds, fd_set *exceptfds, struct timeval *timeout) {
 #ifdef EMSCRIPTEN
-	// TODO: fix this somehow to handle the case where there are humblenet and regular sockets in the same select
-	return humblenet_p2p_wait(0) ? 1 : 0;
+	// NET_GetPacket() only reads sockets marked in readfds. On Emscripten,
+	// signal humblenet readiness by flagging the wrapped UDP socket readable.
+	if( humblenet_p2p_wait(0) ) {
+		if( readfds && g_humblenet_socket != INVALID_SOCKET ) {
+			FD_SET( g_humblenet_socket, readfds );
+		}
+		return 1;
+	}
+	return 0;
 #else
-	return humblenet_p2p_select( nfds, readfds, writefds, exceptfds, timeout );
+	int ret = humblenet_p2p_select( nfds, readfds, writefds, exceptfds, timeout );
+	if( ret > 0 ) {
+		return ret;
+	}
+
+	// HumbleNet may already have queued payload without a live OS fd event.
+	// Surface that as readability for the wrapped UDP socket so NET_GetPacket()
+	// actually calls recvfrom().
+	if( humblenet_p2p_wait(0) ) {
+		if( readfds && g_humblenet_socket != INVALID_SOCKET ) {
+			FD_SET( g_humblenet_socket, readfds );
+		}
+		return 1;
+	}
+	return 0;
 #endif
 }
 
